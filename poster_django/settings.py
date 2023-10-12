9f43d2a91f25151c2dd228a4b8e19181edbecee3
Harivatsa <gaharivatsa@gmail.com>
2023-10-12 20:38:23 +0530

    last

--- poster_django/settings.py
+++ poster_django/settings.py
@@ -82,9 +82,14 @@ WSGI_APPLICATION = "poster_django.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'CvYSyBhOMOC5lQkjl5mM',
+        'HOST': 'containers-us-west-146.railway.app',
+        'PORT': '7741',
     }
 }
 
